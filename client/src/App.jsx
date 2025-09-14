4a57aa6d3ed9587230ea383d9232630a58532ad4
Nouman Ahmed <noumanahmed.cs@gmail.com>
2025-09-14 10:53:47 +0500

    aiControllers completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,15 @@ import GenerateImages from "./pages/GenerateImages";
 import RemoveBackground from "./pages/RemoveBackground";
 import RemoveObject from "./pages/RemoveObject";
 import ReviewResume from "./pages/ReviewResume";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 const App = () => {
+  const { getToken } = useAuth();
+  useEffect(() => {
+    getToken().then((token) => console.log(token));
+  }, []);
+
   return (
     <div>
       <Routes>
--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
+import aiRouter from "./routes/aiRouter.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const app = express();
+
+await connectCloudinary();
+app.use(clerkMiddleware());
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +16,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+app.use(requireAuth());
+
+app.use("/api/ai", aiRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
